a9223ddeb3d03c89e1dfc329a18d1e50cfdcdf9d
Andrii Karpenko <ximerg002@gmail.com>
2023-10-07 16:51:53 +0200

    Added animation

--- SCSUIAnimation/ContentView.swift
+++ SCSUIAnimation/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var presentAnimation: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            presentAnimation.toggle()
+        } label: {
+            AnimatedContainer(isPressed: $presentAnimation)
         }
-        .padding()
+    }
+}
+
+struct AnimatedContainer: View {
+
+    private let image: Image = Image(systemName: "play.fill")
+    @Binding var isPressed: Bool
+
+    var body: some View {
+        HStack(spacing: -12.0) {
+            if isPressed {
+                image.transition(.scale.combined(with: .opacity))
+            }
+
+            image
+
+            if !isPressed {
+                image.transition(.scale.combined(with: .opacity))
+            }
+        }
+        .onChange(of: isPressed, { if !isPressed { isPressed.toggle() } })
+        .animation(isPressed ? .spring(dampingFraction: 0.5) : nil, value: isPressed)
+        .font(.system(size: 100))
     }
 }
 
