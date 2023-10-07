ee612453d76f5893b821d9e97525f9e87da2adf9
Andrii Karpenko <ximerg002@gmail.com>
2023-10-07 13:54:25 +0200

    Initial Commit

---
+++ SCSUIAnimation/SCSUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCSUIAnimationApp.swift
+//  SCSUIAnimation
+//
+//  Created by Andrey Karpenko  on 07.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SCSUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
